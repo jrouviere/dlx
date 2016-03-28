37be1b67d99c948a3764634750ee7d1428526460
Julien Rouviere <julien.rouviere@gmail.com>
2016-03-28 22:34:20 +0100

    Improve tests

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 )
 
-func TestSimple(t *testing.T) {
+func TestBasic2x2(t *testing.T) {
 	mat := newMatrix()
 
 	c0 := mat.createConstraint("C0")
@@ -16,9 +16,56 @@ func TestSimple(t *testing.T) {
 	found, result := mat.solve()
 
 	if found == false {
-		t.Fail()
+		t.Error("Should have found a solution")
 	}
 	if !reflect.DeepEqual(result, []*Cell{r1, r0}) {
-		t.Fail()
+		t.Error("Wrong result")
+	}
+}
+
+
+
+func TestBasic3x3(t *testing.T) {
+	mat := newMatrix()
+
+	c0 := mat.createConstraint("C0")
+	c1 := mat.createConstraint("C1")
+	c2 := mat.createConstraint("C2")
+
+	mat.createChoice("R0", []*Cell{c0})
+	r1 := mat.createChoice("R1", []*Cell{c1})
+	r2 := mat.createChoice("R2", []*Cell{c0, c2})
+
+	found, result := mat.solve()
+
+	if found == false {
+		t.Error("Should have found a solution")
+	}
+	if !reflect.DeepEqual(result, []*Cell{r1, r2}) {
+		t.Error("Wrong result")
+	}
+}
+
+
+func TestBasic4x4(t *testing.T) {
+	mat := newMatrix()
+
+	c0 := mat.createConstraint("C0")
+	c1 := mat.createConstraint("C1")
+	c2 := mat.createConstraint("C2")
+	c3 := mat.createConstraint("C3")
+
+	r0 := mat.createChoice("R0", []*Cell{c0})
+	r1 := mat.createChoice("R1", []*Cell{c1})
+	mat.createChoice("R2", []*Cell{c1, c2})
+	r3 := mat.createChoice("R3", []*Cell{c2, c3})
+
+	found, result := mat.solve()
+
+	if found == false {
+		t.Error("Should have found a solution")
+	}
+	if !reflect.DeepEqual(result, []*Cell{r3, r1, r0}) {
+		t.Error("Wrong result")
 	}
 }
