2a9339b244f60dc1c85c1bd80582d1d112651a18
Julien Rouviere <julien.rouviere@gmail.com>
2016-04-02 11:52:10 +0200

    Add some test cases

--- main_test.go
+++ main_test.go
@@ -88,7 +88,54 @@ func TestBasic4x4(t *testing.T) {
 	if !found {
 		t.Error("Should have found a solution")
 	}
-	if !reflect.DeepEqual(result, []*Cell{r3, r1, r0}) {
-		t.Error("Wrong result")
+	checkSolution(result, []*Cell{r0, r1, r3}, t)
+}
+
+func TestNoSolution(t *testing.T) {
+	mat := newMatrix()
+
+	c0 := mat.createConstraint("C0")
+	mat.createConstraint("C1")
+	mat.createChoice("R0", []*Cell{c0})
+	mat.createChoice("R1", []*Cell{c0})
+
+	found, result := mat.solve()
+
+	if found {
+		t.Error("Solution should have not been found")
 	}
+	checkSolution(result, []*Cell{}, t)
+}
+
+func TestCover(t *testing.T) {
+
+	/* sample from wikipedia
+	A = {1, 4, 7}
+	B = {1, 4}
+	C = {4, 5, 7}
+	D = {3, 5, 6}
+	E = {2, 3, 6, 7}
+	F = {2, 7}
+	=> solution is B, D, F
+	*/
+
+	mat := newMatrix()
+
+	ctr := make([]*Cell, 7)
+	for i := 0; i < 7; i++ {
+		ctr[i] = mat.createConstraint(i + 1)
+	}
+
+	mat.createChoice("A", []*Cell{ctr[0], ctr[3], ctr[6]})
+	B := mat.createChoice("B", []*Cell{ctr[0], ctr[3]})
+	mat.createChoice("C", []*Cell{ctr[3], ctr[4], ctr[6]})
+	D := mat.createChoice("D", []*Cell{ctr[2], ctr[4], ctr[5]})
+	mat.createChoice("E", []*Cell{ctr[1], ctr[2], ctr[5], ctr[6]})
+	F := mat.createChoice("F", []*Cell{ctr[1], ctr[6]})
+
+	found, result := mat.solve()
+	if !found {
+		t.Error("Should have found a solution")
+	}
+	checkSolution(result, []*Cell{B, D, F}, t)
 }
